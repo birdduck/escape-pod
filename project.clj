f8307a02f03f44bbda63853c5af9d8886681918b
Colin Teal <the30yearswar@hotmail.com>
2018-06-28 22:14:04 -0700

    Update org.clojure/clojurescript to 1.10.339

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [markdown-clj "1.0.2"
                   :exclusions [org.clojure/clojure]]
                  [org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.238"]
+                 [org.clojure/clojurescript "1.10.339"]
                  [org.clojure/tools.cli "0.3.7"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]]
