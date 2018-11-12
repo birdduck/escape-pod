e1bc652bda011a22f9062b52d8e4ef65e6576a36
Colin Teal <the30yearswar@hotmail.com>
2018-11-12 15:38:20 -0800

    Update markdown-clj to 1.0.5

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [funcool/promesa "1.9.0"]
                  [garden "1.3.6"]
                  [hiccups "0.3.0"]
-                 [markdown-clj "1.0.3"
+                 [markdown-clj "1.0.5"
                   :exclusions [org.clojure/clojure]]
                  [org.clojure/clojure "1.10.0-beta5"]
                  [org.clojure/clojurescript "1.10.439"]
