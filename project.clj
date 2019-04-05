022ee08f692bd6701a97b55d62935bc1479b16ac
Colin Teal <the30yearswar@hotmail.com>
2019-04-05 00:24:46 -0700

    Update funcool/cuerdas to 2.2.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject escape-pod "0.0.1"
   :dependencies [[cljsjs/nodejs-externs "1.0.4-1"]
                  [cljsjs/twemoji "11.2.0-0"]
-                 [funcool/cuerdas "2.0.6"]
+                 [funcool/cuerdas "2.2.0"]
                  [funcool/promesa "1.9.0"]
                  [garden "1.3.6"]
                  [hiccups "0.3.0"]
