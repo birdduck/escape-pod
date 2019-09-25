82f695ba435ba03333b763e6648db9a2205f8922
Colin Teal <the30yearswar@hotmail.com>
2019-09-25 15:28:37 -0700

    Update funcool/promesa to 3.0.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
                  [cljsjs/nodejs-externs "1.0.4-1"]
                  [cljsjs/twemoji "11.2.0-0"]
                  [funcool/cuerdas "2.2.0"]
-                 [funcool/promesa "2.0.1"]
+                 [funcool/promesa "3.0.0"]
                  [garden "1.3.6"]
                  [hiccups "0.3.0"]
                  [markdown-clj "1.10.0"
